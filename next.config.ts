80c42f1e03112cb502ad4e1a8c9221f829905a69
raffimahya <raffimlbb.23@gmail.com>
2025-07-09 17:47:08 +0700

    updt img

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+      images: {
+    domains: ['zqksipwraavhujicsndu.supabase.co'],
+  },
   async headers() {
     const securityHeaders = [
       {
